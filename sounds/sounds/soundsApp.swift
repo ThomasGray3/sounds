7885e6328f56a7c6ea2f176e76ed3fb4cf429a08
Thomas Gray <tomalexander1999@googlemail.com>
2025-09-20 18:35:06 +0100

    creating project

---
+++ sounds/sounds/soundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  soundsApp.swift
+//  sounds
+//
+//  Created by Thomas Gray on 20/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct soundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sounds/soundsTests/soundsTests.swift
@@ -0,0 +1,17 @@
+//
+//  soundsTests.swift
+//  soundsTests
+//
+//  Created by Thomas Gray on 20/09/2025.
+//
+
+import Testing
+@testable import sounds
+
+struct soundsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
